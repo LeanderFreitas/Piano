e35ec4fd171b8df132ea5e6153480bd9663b548b
Leander Freitas <leanderfreitas18@gmail.com>
2025-07-14 20:44:33 -0300

    .

--- engine.js
+++ engine.js
@@ -1,7 +1,10 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio("./tunes/a.wav");
-const playTune = (key) => {};
+let audio = new Audio("src/tunes/a.wav");
+const playTune = (key) => {
+    audio.src = 'src/tunes/${key}.wav';
+    audio.play();
+};
 
 pianoKeys.forEach((key) => {
      console.log(key.dataset.key);
